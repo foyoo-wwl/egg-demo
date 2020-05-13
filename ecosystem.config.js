f525af816d05f98fd75acb24f2ca9a931af58cc1
wwl6324 <411404821@qq.com>
2020-05-13 18:48:52 +0800

    提交修改

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       user: 'root', // 用户，一般都是填写 root 权限最高
       host: '39.105.132.152', // 服务器 IP 地址，大家可以看看自己的云服务器 IP 地址
       ref: 'origin/master', // 获取代码库的分支，我这边就拉取主分支代码
-      repo: 'git@github.com:foyoo-wwl/egg-demo.git', // 仓库地址
+      repo: 'https://github.com/foyoo-wwl/egg-demo.git', // 仓库地址
       path: '/workspace/egg-demo', // 项目在服务器的存放地址
       'post-deploy': 'git pull && npm install && npm run start', // 在本地运行 pm2 的时候，在项目根目录需要运行的命令行
       env: {
